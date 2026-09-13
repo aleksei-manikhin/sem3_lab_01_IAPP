95afff5a0422d4ce440628619270c7ebce488741
aleksei-manikhin <alekseimanikhin@gmail.com>
2026-09-14 02:01:20 +0300

    add models.py

---
+++ models.py
@@ -0,0 +1,25 @@
+from typing import Dict, List, Tuple, Union
+
+
+YEAR_COLUMN = "year"
+REGION_COLUMN = "region"
+
+COLUMN_NAMES = (
+    YEAR_COLUMN,
+    REGION_COLUMN,
+    "npg",
+    "birth_rate",
+    "death_rate",
+    "gdw",
+    "urbanization",
+)
+
+NUMERIC_COLUMNS = tuple(
+    name for name in COLUMN_NAMES if name != REGION_COLUMN
+)
+
+CellValue = Union[str, int, float]
+DataRow = Dict[str, CellValue]
+Dataset = List[DataRow]
+Statistics = Dict[str, float]
+Percentiles = List[Tuple[int, float]]
